5665da2465b152eaee3cb03744e73c38a83028f2
Sajeyks <sajeyksmwangi@gmail.com>
2023-01-29 09:14:02 +0300

    add register models

--- H_security/p_records/admin.py
+++ H_security/p_records/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import LabTest, Diagnosis, Prescription, PreexistingCondition, HealthRecord, HospitalVisit
 # Register your models here.
+
+admin.site.register(LabTest)
+admin.site.register(Diagnosis)
+admin.site.register(Prescription)
+admin.site.register(PreexistingCondition)
+admin.site.register(HealthRecord)
+admin.site.register(HospitalVisit)
