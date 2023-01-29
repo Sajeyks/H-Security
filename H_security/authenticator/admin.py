bc957babb31f89c314d01a26dab1693f4baf0f88
Sajeyks <sajeyksmwangi@gmail.com>
2023-01-29 09:03:06 +0300

    register user model

--- H_security/authenticator/admin.py
+++ H_security/authenticator/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import User, Profile
 # Register your models here.
 
-admin.site.register(User)
+class User_Admin(admin.ModelAdmin):
+    list_display = ["email","age"]
+    # readonly_fields = ["hero_image"]
+    # prepopulated_fields = {"slug": ("Title",)}
+
+admin.site.register(User,User_Admin)
 admin.site.register(Profile)
