6392b7adebcf95fa4abaafde2ee77bb697c1ed45
Sajeyks <sajeyksmwangi@gmail.com>
2023-02-06 13:11:25 +0300

    register bills

--- H_security/p_records/admin.py
+++ H_security/p_records/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import LabTest, Diagnosis, Prescription, PreexistingCondition, HealthRecord, HospitalVisit
+from .models import LabTest, Diagnosis, Prescription, PreexistingCondition, HealthRecord, HospitalVisit, Bill
 # Register your models here.
 
 admin.site.register(LabTest)
@@ -8,3 +8,4 @@ admin.site.register(Prescription)
 admin.site.register(PreexistingCondition)
 admin.site.register(HealthRecord)
 admin.site.register(HospitalVisit)
+admin.site.register(Bill)
