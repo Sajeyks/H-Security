f6ac1496dbb34e3856d49890a032cf1d3dee273b
Sajeyks <sajeyksmwangi@gmail.com>
2023-02-11 03:25:06 +0300

    moved to PostgreSQL

--- H_security/H_security/settings.py
+++ H_security/H_security/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'H_security.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Hospital',
+        'USER': 'postgres', 
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
