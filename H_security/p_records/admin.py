d02e54311a16849a38d9dae51ac6b47e2d0fd3dc
Sajeyks <sajeyksmwangi@gmail.com>
2023-02-11 03:32:48 +0300

    deregister pre-conditions

--- H_security/p_records/admin.py
+++ H_security/p_records/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import LabTest, Diagnosis, Prescription, PreexistingCondition, HealthRecord, HospitalVisit, Bill
+from .models import LabTest, Diagnosis, Prescription, HealthRecord, HospitalVisit, Bill
 # Register your models here.
 
 admin.site.register(LabTest)
 admin.site.register(Diagnosis)
 admin.site.register(Prescription)
-admin.site.register(PreexistingCondition)
 admin.site.register(HealthRecord)
 admin.site.register(HospitalVisit)
 admin.site.register(Bill)
