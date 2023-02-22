1a32738b7a1e7d7e2c0921f5a8a5d7639e181f90
Sajeyks <sajeyksmwangi@gmail.com>
2023-02-22 03:05:38 +0300

    remove unused choices

--- H_security/p_records/choices.py
+++ H_security/p_records/choices.py
@@ -1,18 +1,5 @@
 from django.utils.translation import gettext as _
 
-PRESCRIPTION_TYPE = (
-    ("Acute (not to be repeated)", _("Acute (not to be repeated)")),
-    ("Repeat (long term medication)", _("Repeat (long term medication)")),
-    ("Repeat Dispensing (specific amount of ussues)", _("Repeat Dispensing (specific amount of ussues)")),
-     )
-
-MEDICINE_TYPE = (
-    ("Pain relievers", _("Pain relievers")),
-    ("Tranquilizers", _("Tranquilizers")),
-    ("Stimulants", _("Stimulants")),
-    ("Sedatives", _("Sedatives"))
-     )
-
 PAYMENT_OPTIONS = (
     ("NHIF", _("NHIF")),
     ("INSURANCE COMPANY", _("INSURANCE COMPANY")),
