4c14cafbdc5d09926d1a7f42cc21e0173634f28b
Sajeyks <sajeyksmwangi@gmail.com>
2023-02-23 00:58:02 +0300

    modify models:   - encrypt sensitive data   - change healthrecord relationship with owner to onetoone

--- H_security/p_records/models.py
+++ H_security/p_records/models.py
@@ -3,14 +3,15 @@ from .choices import *
 from django.contrib.auth import get_user_model
 from djmoney.models.fields import MoneyField
 User = get_user_model()
+from django_cryptography.fields import encrypt
 
 # Create your models here.
 
 class HospitalVisit(models.Model):
-    tests = models.TextField(max_length=200, blank=True)
-    diagnosis = models.TextField(max_length=200, blank=True)
-    prescriptions = models.TextField(max_length=200, blank=True)
-    edited_by = models.ManyToManyField(User, related_name="h_edited_by", blank=True)
+    tests = encrypt(models.TextField(max_length=200))
+    diagnosis = encrypt(models.TextField(max_length=200))
+    prescriptions = encrypt(models.TextField(max_length=200))
+    edited_by = models.ManyToManyField(User, related_name="h_edited_by")
     date_recorded = models.DateField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     bill = MoneyField(max_digits=14, decimal_places=2, default_currency='KES', default=('0.0'))
@@ -29,8 +30,8 @@ class HospitalVisit(models.Model):
         return set(hospitals)
     
 class HealthRecord(models.Model):
-    owner = models.ForeignKey(User, on_delete= models.CASCADE)
-    pre_existing_conditions = models.TextField(max_length=200, blank=True)
+    owner = models.OneToOneField(User, on_delete= models.CASCADE)
+    pre_existing_conditions = encrypt(models.TextField(max_length=200, blank=True))
     hospital_visits = models.ManyToManyField(HospitalVisit, related_name="h_hospital_visits")
     
     def __str__(self):
