68502fb98e411a05cf8bf60e8f52e5333631a61f
Sajeyks <sajeyksmwangi@gmail.com>
2023-02-22 14:29:19 +0300

    add visit deletion url

--- H_security/p_records/urls.py
+++ H_security/p_records/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import healthRecord, recordDetails, hospitaVisit, hospitaVisitDetails, addHospitalVisit
+from .views import healthRecord, recordDetails, hospitaVisit, hospitaVisitDetails, addHospitalVisit, deleteHospitalVisit
 # from .forms import LoginForm
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('record-details/<int:pk>',  recordDetails, name='records-details'),
     path('visit-details/<int:pk>',  hospitaVisitDetails, name='visit-details'),
     path('<int:pk>/add-visit/', addHospitalVisit, name='visit_submit'),
+    path('<int:pk>/delete-visit/', deleteHospitalVisit, name='visit_delete'),
+    
 ]
