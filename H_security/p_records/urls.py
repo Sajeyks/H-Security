8b2dd0d47fdacb2a57b63d214be1cca85618a957
Sajeyks <sajeyksmwangi@gmail.com>
2023-02-22 12:48:06 +0300

    add hospital visit creation url

--- H_security/p_records/urls.py
+++ H_security/p_records/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import healthRecord, recordDetails, hospitaVisit, hospitaVisitDetails
+from .views import healthRecord, recordDetails, hospitaVisit, hospitaVisitDetails, addHospitalVisit
 # from .forms import LoginForm
 
 urlpatterns = [
     path('health-records/',  healthRecord, name='h-records'),
     path('record-details/<int:pk>',  recordDetails, name='records-details'),
     path('visit-details/<int:pk>',  hospitaVisitDetails, name='visit-details'),
+    path('<int:pk>/add-visit/', addHospitalVisit, name='visit_submit'),
 ]
