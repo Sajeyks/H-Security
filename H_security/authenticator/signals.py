85b7e1a1de281a40387d69b2251c131fe21135f7
Sajeyks <sajeyksmwangi@gmail.com>
2023-03-06 12:39:50 +0300

    add group-assigning signal

--- H_security/authenticator/signals.py
+++ H_security/authenticator/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from django.dispatch import receiver
 from .models import Profile
 
@@ -13,3 +14,15 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+    
+
+@receiver(post_save, sender=User)
+def assign_group(sender, instance, created, **kwargs):
+    if created:
+        email = instance.email
+        if email.endswith('@hospital.staff.go.ke'):
+            group, created = Group.objects.get_or_create(name='Hospital Staff Group')
+            instance.groups.add(group)
+        else:
+            group, created = Group.objects.get_or_create(name='Non-Staff Group')
+            instance.groups.add(group)
